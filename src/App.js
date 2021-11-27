34500619f6d0b8fb2517e19d05efb1059edf49ec
Kesar <cesar.huret@gmail.com>
2021-11-27 17:32:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import Web3 from 'web3';
+
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 
-  constructor() {
-    super()
-    this.web3 = new Web3(window.ethereum);
-    this.state = {
-      account: '',
+    constructor() {
+      super()
+      this.state = {
+        account: '',
+      }
     }
-  }
-
-  async componentDidMount() {
-    try{ 
-      const accounts =  await window.ethereum.request({method: 'eth_requestAccounts'})
-      this.setState({ account: accounts[0] })
-    } catch (e) {
-      console.log(e)
+  
+    async componentDidMount() {
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      console.log(accounts[0])
+      this.setState({account: accounts[0]})
     }
-  }
 
-  render() {
-    console.log(this.state.account)
-    return (
-    <div className="App">
-      <h1>
-        {this.state.account}
-      </h1>
-    </div>
-    );
-  }
+    render() {
+      return (
+      <div className="App">
+        <h1>
+          {this.state.account}
+        </h1>
+      </div>
+      );
+    }
 }
 
 export default App;
